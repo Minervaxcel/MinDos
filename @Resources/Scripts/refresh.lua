caf66edbf57e6be7cf2acf0a6942b2d2a331d08a
Niklas Jørgensen <minervaxcel@gmail.com>
2016-05-04 23:27:53 +0200

    [MinDos] Move scripts to #@#
    Fix Fetchers having the wrong include path too.

--- @Resources/Scripts/refresh.lua
+++ @Resources/Scripts/refresh.lua
@@ -4,26 +4,26 @@ function Initialize()
    StartingMinutes = SKIN:GetVariable('RemainingMinutes')
    StartingSeconds = SKIN:GetVariable('RemainingSeconds2')
    
-   TotalSeconds = (tonumber(StartingHours) * 3600) + (tonumber(StartingMinutes) * 60) + tonumber(StartingSeconds)
-   SKIN:Bang('!SetOption', 'MeasureBarTime', 'MaxValue', TotalSeconds)
+   TotalSeconds2 = (tonumber(StartingHours) * 3600) + (tonumber(StartingMinutes) * 60) + tonumber(StartingSeconds)
+   SKIN:Bang('!SetOption', 'MeasureTimer', 'MaxValue', TotalSeconds2)
    
 end --> Initialize
 
 function Update()
 
-   TotalSeconds = TotalSeconds - 1
+   TotalSeconds2 = TotalSeconds2 - 1
 
-   NowHours = math.floor(TotalSeconds / 3600)
-   HoursRemainder = math.floor(TotalSeconds % 3600)
+   NowHours = math.floor(TotalSeconds2 / 3600)
+   HoursRemainder = math.floor(TotalSeconds2 % 3600)
    NowMinutes = math.floor(HoursRemainder / 60)
-   NowSeconds = math.floor(TotalSeconds % 60)
+   NowSeconds = math.floor(TotalSeconds2 % 60)
 
    SKIN:Bang('!SetVariable', 'RemainingHours', NowHours)
    SKIN:Bang('!SetVariable', 'RemainingMinutes', NowMinutes)
    SKIN:Bang('!SetVariable', 'RemainingSeconds2', NowSeconds)
-   SKIN:Bang('!SetOption', 'MeasureBarTime', 'Formula', ((NowHours*3600)+(NowMinutes*60)+NowSeconds))
+   SKIN:Bang('!SetOption', 'MeasureTimer', 'Formula', ((NowHours*3600)+(NowMinutes*60)+NowSeconds))
    
-   if TotalSeconds <= 0 then
+   if TotalSeconds2 <= 0 then
 	  SKIN:Bang('!Refresh')
    end      
    
